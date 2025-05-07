64f368bf158b0f3f29c0a7b58a331ec1430c7504
David Tung <dsonic@gmail.com>
2025-05-07 14:50:30 -0700

    Fixed buid errors on Vercel

--- nextAdminOptions.tsx
+++ nextAdminOptions.tsx
@@ -1,9 +1,46 @@
 import type { NextAdminOptions } from "@premieroctet/next-admin";
 
 const options: NextAdminOptions = {
-  title: "My awesome admin",
-
-  model: {}
-}
+  title: "Resume Builder Admin",
+  model: {
+    Resume: {
+      title: "Resumes",
+      list: {
+        fields: {
+          id: {},
+          title: {},
+          fullName: {},
+          location: {},
+        },
+      },
+      edit: {
+        fields: {
+          id: {
+            disabled: true,
+          },
+          title: {
+            required: true,
+          },
+          fullName: {
+            required: true,
+          },
+          location: {},
+          about: {
+            format: "textarea",
+          },
+          summary: {
+            format: "textarea",
+          },
+          avatarUrl: {
+            format: "uri",
+          },
+          personalWebsiteUrl: {
+            format: "uri",
+          },
+        },
+      },
+    },
+  },
+};
 
 export default options;
